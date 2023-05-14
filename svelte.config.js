37dfa3be6c5bb1cd1e90a280ff7631d18026c269
Francesco Dessì <francesco.dessi@outlook.com>
2023-05-14 12:21:15 +0200

    Updating svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            pages: 'docs',
-            assets: 'docs',
+            pages: 'build',
+            assets: 'build',
             fallback: null,
             precompress: false,
             strict: true
