297224bc84cc35913614c127b1372700bb7fcadf
Francesco Dessì <francesco.dessi@outlook.com>
2023-05-14 12:26:46 +0200

    svelte adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
